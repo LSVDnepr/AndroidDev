c341ccd74c8d5914d8ce6fad94dc4569dadca2bc
Svetlana Litovchenko <litovchenkosv1990@gmail.com>
2018-04-16 01:44:16 +0300

    Провела масштабный рефакторинг кода

--- MyCalcV2_0/app/src/main/java/com/androidstudy/lsvhome/mycalcv2_0/ButtonClickedListener.java
+++ MyCalcV2_0/app/src/main/java/com/androidstudy/lsvhome/mycalcv2_0/ButtonClickedListener.java
@@ -1,8 +1,13 @@
 package com.androidstudy.lsvhome.mycalcv2_0;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class ButtonClickedListener implements View.OnClickListener {
     private TextView textView;
@@ -10,16 +15,138 @@ public class ButtonClickedListener implements View.OnClickListener {
     private AppCompatActivity activity;
 
 
-    public ButtonClickedListener(TextView textView, AppCompatActivity activity){
-        this.textView=textView;
-        this.activity=activity;
+    public ButtonClickedListener(TextView textView, AppCompatActivity activity) {
+        this.textView = textView;
+        this.activity = activity;
     }
 
 
     @Override
     public void onClick(View v) {
+        Button btn = (Button) v;
+        switch (v.getId()) {
+            case R.id.buttonMC:
+                memory = "";
+                break;
+            case R.id.buttonMS:
+                memory = textView.getText().toString();
+                break;
+            case R.id.buttonMR:
+                textView.setText(memory);
+                break;
+            case R.id.buttonC:
+                textView.setText("0");
+                break;
+            case R.id.buttonDel:
+                CharSequence cs = textView.getText();
+                if (cs.length() == 1) {
+                    textView.setText("0");
+                    return;
+                }
+                textView.setText(cs.subSequence(0, cs.length() - 1));
+                break;
+            case R.id.buttonPlus:
+            case R.id.buttonMinus:
+            case R.id.buttonMultiply:
+            case R.id.buttonDivide:
+            case R.id.buttonDot:
+                operatorClicked(btn.getText().toString());
+                break;
+            case R.id.buttonPth1://прописать логику
+                break;
+            case R.id.buttonPth2://прописать логику
+                break;
+            case R.id.buttonEquals:
+                textView.setText(calcResult(textView.getText().toString()));
 
-
+                //new
+                Intent intent = new Intent(activity, SecondActivity.class);
+                intent.putExtra("calcResults", textView.getText().toString());
+                activity.startActivityForResult(intent, 0);
+                break;
+            default:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText(((Button) v).getText());
+                    return;
+                }
+                textView.append(((Button) v).getText());
+        }
 
     }
+
+
+    public void operatorClicked(String btnText) {
+        Character[] replace = new Character[]{'.', '+', '-', '*', '/'};
+        CharSequence calcAreaText = textView.getText();
+        int lastInd = calcAreaText.length() - 1;
+
+        for (Character ch : replace) {
+            if (ch.equals(calcAreaText.charAt(lastInd))) {
+                return;
+            }
+        }
+
+        textView.append(btnText);
+    }
+
+    public String calcResult(String calcStr) {
+        try {
+            StringTokenizer sT = new StringTokenizer(calcStr, "+-*/", true);
+            if (sT.countTokens() % 2 == 0) {
+                return "error";
+            }
+            ArrayList<String> el = new ArrayList<>();
+            while (sT.hasMoreTokens()) {
+                el.add(sT.nextToken());
+            }
+
+            Double.parseDouble(el.get(0));
+            Double.parseDouble(el.get(el.size() - 1));
+
+            for (int i = 1; i < el.size() - 1; i++) {
+                if (el.get(i).equals("*")) {
+                    double res = Double.parseDouble(el.get(i - 1)) * Double.parseDouble(el.get(i + 1));
+                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
+                    el.set(i, String.valueOf(res));
+                    el.remove(i - 1);
+                    el.remove(i--);
+                    continue;
+                }
+
+                if (el.get(i).equals("/")) {
+                    double res = Double.parseDouble(el.get(i - 1)) / Double.parseDouble(el.get(i + 1));
+                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
+                    el.set(i, String.valueOf(res));
+                    el.remove(i - 1);
+                    el.remove(i--);
+                }
+            }
+
+            for (int i = 1; i < el.size() - 1; i++) {
+                if (el.get(i).equals("+")) {
+                    double res = Double.parseDouble(el.get(i - 1)) + Double.parseDouble(el.get(i + 1));
+                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
+                    el.set(i, String.valueOf(res));
+                    el.remove(i - 1);
+                    el.remove(i--);
+                    continue;
+                }
+
+                if (el.get(i).equals("-")) {
+                    double res = Double.parseDouble(el.get(i - 1)) - Double.parseDouble(el.get(i + 1));
+                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
+                    el.set(i, String.valueOf(res));
+                    el.remove(i - 1);
+                    el.remove(i--);
+                }
+
+            }
+            return el.get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error";
+        }
+    }
+
+
 }
--- MyCalcV2_0/app/src/main/java/com/androidstudy/lsvhome/mycalcv2_0/MainActivity.java
+++ MyCalcV2_0/app/src/main/java/com/androidstudy/lsvhome/mycalcv2_0/MainActivity.java
@@ -5,18 +5,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView calcStr;//=(TextView)findViewById(R.id.calcArea);//есть ли риски такого присвоения в качестве переменной класса?
-    public String memory = "";
-    private MainActivity mainActivity=this;
+    private TextView calcStr;
 
+    public void setButtonListeners(View v, View.OnClickListener listener){
+        if (v instanceof Button){
+            v.setOnClickListener(listener);
+            return;
+        }
+        if (!(v instanceof ViewGroup)){
+            return;
+        }
+        for (int i=0;i<((ViewGroup) v).getChildCount();i++){
+            setButtonListeners(((ViewGroup) v).getChildAt(i),listener);
+        }
 
+    }
 
 
     @Override
@@ -25,191 +34,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       calcStr = (TextView) findViewById(R.id.calcArea);
+        calcStr = (TextView) findViewById(R.id.calcArea);
 
-
-
-
-
-        NumPressedListener numListener = new NumPressedListener(calcStr);
-        OperatorPressedListener operatorPressed = new OperatorPressedListener(calcStr);
-
-        Button b1 = (Button) findViewById(R.id.button1);
-        b1.setOnClickListener(numListener);
-
-        Button b2 = (Button) findViewById(R.id.button2);
-        b2.setOnClickListener(numListener);
-
-        Button b3 = (Button) findViewById(R.id.button3);
-        b3.setOnClickListener(numListener);
-
-        Button b4 = (Button) findViewById(R.id.button4);
-        b4.setOnClickListener(numListener);
-
-        Button b5 = (Button) findViewById(R.id.button5);
-        b5.setOnClickListener(numListener);
-
-        Button b6 = (Button) findViewById(R.id.button6);
-        b6.setOnClickListener(numListener);
-
-        Button b7 = (Button) findViewById(R.id.button7);
-        b7.setOnClickListener(numListener);
-
-        Button b8 = (Button) findViewById(R.id.button8);
-        b8.setOnClickListener(numListener);
-
-        Button b9 = (Button) findViewById(R.id.button9);
-        b9.setOnClickListener(numListener);
-
-        Button b0 = (Button) findViewById(R.id.button0);
-        b0.setOnClickListener(numListener);
-
-        Button bPlus = (Button) findViewById(R.id.buttonPlus);
-        bPlus.setOnClickListener(operatorPressed);
-
-        Button bMinus = (Button) findViewById(R.id.buttonMinus);
-        bMinus.setOnClickListener(operatorPressed);
-
-        Button bMultiply = (Button) findViewById(R.id.buttonMultiply);
-        bMultiply.setOnClickListener(operatorPressed);
-
-        Button bDivide = (Button) findViewById(R.id.buttonDivide);
-        bDivide.setOnClickListener(operatorPressed);
-
-       /* Button bPth1=(Button)findViewById(R.id.buttonPth1);
-        bPth1.setOnClickListener(operatorPressed);
-
-        Button bPth2=(Button)findViewById(R.id.buttonPth2);
-        bPth2.setOnClickListener(operatorPressed);*/
-
-        Button bDot = (Button) findViewById(R.id.buttonDot);
-        bDot.setOnClickListener(operatorPressed);
-
-
-        Button bDel = (Button) findViewById(R.id.buttonDel);
-        bDel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                CharSequence cs = calcStr.getText();
-                if (cs.length() == 1) {
-                    calcStr.setText("0");
-                    return;
-                }
-                calcStr.setText(cs.subSequence(0, cs.length() - 1));
-            }
-        });
-
-        Button bClear = (Button) findViewById(R.id.buttonC);
-        /*ClipDrawable drawable=(ClipDrawable)bClear.getBackground();
-        drawable.setLevel(5000);*/
-        bClear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calcStr.setText("0");
-            }
-        });
-
-        Button bMC = (Button) findViewById(R.id.buttonMC);
-        bMC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                memory = "";
-            }
-        });
-
-
-        Button bMS = (Button) findViewById(R.id.buttonMS);
-        bMS.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                memory = calcStr.getText().toString();
-            }
-        });
-
-
-        Button bMR = (Button) findViewById(R.id.buttonMR);
-        bMR.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calcStr.setText(memory);
-            }
-        });
-
-        Button bEquals = (Button) findViewById(R.id.buttonEquals);
-        bEquals.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                calcStr.setText(calcResult(calcStr.getText().toString()));
-
-                //new
-                Intent intent = new Intent(mainActivity,SecondActivity.class);
-                intent.putExtra("calcResults", calcStr.getText().toString());
-                startActivityForResult(intent, 0);
-
-            }
-        });
+        ButtonClickedListener btnListener=new ButtonClickedListener(calcStr,this);
+        setButtonListeners(findViewById(R.id.buttonLayout),btnListener);
 
     }
 
 
-    public String calcResult(String calcStr) {
-        try {
-            StringTokenizer sT = new StringTokenizer(calcStr, "+-*/", true);
-            if (sT.countTokens() % 2 == 0) {
-                return "error";
-            }
-            ArrayList<String> el = new ArrayList<>();
-            while (sT.hasMoreTokens()) {
-                el.add(sT.nextToken());
-            }
 
-            Double.parseDouble(el.get(0));
-            Double.parseDouble(el.get(el.size() - 1));
-
-            for (int i = 1; i < el.size() - 1; i++) {
-                if (el.get(i).equals("*")) {
-                    double res = Double.parseDouble(el.get(i - 1)) * Double.parseDouble(el.get(i + 1));
-                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
-                    el.set(i, String.valueOf(res));
-                    el.remove(i - 1);
-                    el.remove(i--);
-                    continue;
-                }
-
-                if (el.get(i).equals("/")) {
-                    double res = Double.parseDouble(el.get(i - 1)) / Double.parseDouble(el.get(i + 1));
-                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
-                    el.set(i, String.valueOf(res));
-                    el.remove(i - 1);
-                    el.remove(i--);
-                }
-            }
-
-            for (int i = 1; i < el.size() - 1; i++) {
-                if (el.get(i).equals("+")) {
-                    double res = Double.parseDouble(el.get(i - 1)) + Double.parseDouble(el.get(i + 1));
-                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
-                    el.set(i, String.valueOf(res));
-                    el.remove(i - 1);
-                    el.remove(i--);
-                    continue;
-                }
-
-                if (el.get(i).equals("-")) {
-                    double res = Double.parseDouble(el.get(i - 1)) - Double.parseDouble(el.get(i + 1));
-                    //el.set(i,String.valueOf(Double.parseDouble(el.get(i-1))*Double.parseDouble(el.get(i+1))));
-                    el.set(i, String.valueOf(res));
-                    el.remove(i - 1);
-                    el.remove(i--);
-                }
-
-            }
-            return el.get(0);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "error";
-        }
-    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
